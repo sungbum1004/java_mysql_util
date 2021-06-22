953aa81526eb2411a6b23ca2c242151c48ec383b
sungbum1004 <tlsqkfka96@naver.com>
2021-06-22 10:03:38 +0900

    MysqlUtil 라이브러리에서 파싱한 데이터를 Map이 아닌 특정 객체 형태로 만드는 기능 추가

--- src/com/jsb/mysqliutil/Test.java
+++ src/com/jsb/mysqliutil/Test.java
@@ -17,13 +17,34 @@ public class Test {
 		// jacksonTest1();
 
 		// 리스트 파싱 테스트 1
-		//jacksonListTest1();
+		// jacksonListTest1();
 		// 리스트 파싱 테스트 2
-		//jacksonListTest2();
+		// jacksonListTest2();
 		// 리스트 파싱 테스트 3
-		//jacksonListTest3();
+		// jacksonListTest3();
 		// 리스트 파싱 테스트 4
-		jacksonListTest4();
+		// jacksonListTest4();
+
+				mysqlUtilJacksonTest();
+			}
+
+			private static void mysqlUtilJacksonTest() {
+				MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "mysqlutil");
+
+				MysqlUtil.setDevMode(true);
+
+				// select rows
+				SecSql sql1 = new SecSql();
+				sql1.append("SELECT * FROM article ORDER BY id DESC");
+
+				List<Article> articleList = MysqlUtil.selectRows(sql1, Article.class);
+				System.out.println("articleList : " + articleList);
+
+				// select row
+				SecSql sql2 = new SecSql();
+				sql2.append("SELECT * FROM article WHERE id = ?", 1);
+				Article article = MysqlUtil.selectRow(sql2, Article.class);
+				System.out.println("article : " + article);
 	}
 
 	private static void jacksonListTest4() throws JsonMappingException, JsonProcessingException {
@@ -63,6 +84,7 @@ public class Test {
 
 		List<Map<String, Object>> list1 = om.readValue(jsonStr1, new TypeReference<List<Map<String, Object>>>() {
 		});
+		
 		System.out.println(list1);
 		System.out.println(list1.get(0));
 		System.out.println(list1.get(0).get("id"));
@@ -104,8 +126,8 @@ public class Test {
 		// select rows
 		SecSql sql1 = new SecSql();
 		sql1.append("SELECT * FROM article ORDER BY id DESC");
-		List<Map<String, Object>> articleListMap = MysqlUtil.selectRows(sql1);
-		System.out.println("articleListMap : " + articleListMap);
+		List<Map<String, Object>> articleMapList = MysqlUtil.selectRows(sql1);
+		System.out.println("articleMapList : " + articleMapList);
 		// select row
 		SecSql sql2 = new SecSql();
 		sql2.append("SELECT * FROM article WHERE id = ?", 1);
